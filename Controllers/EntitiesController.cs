560e2f4b7bd79c80cb9d11b599544d372db2ff0c
Ashish Thakur <50015357+AshishThakur10@users.noreply.github.com>
2024-03-10 23:59:19 +0530

    Add pagination

--- Controllers/EntitiesController.cs
+++ Controllers/EntitiesController.cs
@@ -4,34 +4,63 @@ using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
 [Route("api/[controller]")]
-public class EntitiesController: ControllerBase{
+public class EntitiesController : ControllerBase
+{
 
     [HttpPost]
-    public string CreateEntities(){
+    public string CreateEntities()
+    {
         return "Create a new Entities";
     }
 
     [HttpGet]
     [Route("All", Name = "GetAllEntities")]
-    public IEnumerable<Entity> GetAllEntities(){
-        return MockDatabase.Entities;
+    public ActionResult<IEnumerable<Entity>> GetAllEntities([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+    {
+        if (pageNumber <= 0 || pageSize <= 0)
+        {
+            return BadRequest("Invalid pagination parameters. pageNumber and pageSize must be positive integers.");
+        }
+        int totalEntities = MockDatabase.Entities.Count();
+        int totalPages = (int)Math.Ceiling((double)totalEntities / pageSize);
+
+        if (pageNumber > totalPages)
+        {
+            return NotFound("The requested page number is greater than the total number of pages.");
+        }
+        var paginatedEntities = MockDatabase.Entities
+        .Skip((pageNumber-1) * pageSize)
+        .Take(pageSize)
+        .ToList();
+
+        var response = new {
+            Data = paginatedEntities,
+            PageNumber = pageNumber,
+            pageSize  = pageSize,
+            TotalCount = totalEntities
+        };
+
+        return Ok(response);
     }
 
     [HttpGet("{Id:alpha}", Name = "GetEntitiyByID")]
-    public Entity GetEntitiyByID(string Id){
+    public ActionResult<Entity> GetEntitiyByID(string Id)
+    {
         var data = MockDatabase.Entities.FirstOrDefault(e => e.Id == Id);
         return data;
     }
 
     [HttpPut]
-    [Route("{Id:alpha}")]
-    public string UpdateEntities(string Id){
-        return "Update Entities by ID "+ Id;
+    [Route("{Id:alpha}", Name = "UpdateById")]
+    public string UpdateEntities(string Id)
+    {
+        return "Update Entities by ID " + Id;
     }
 
     [HttpDelete]
-    [Route("{Id:alpha}")]
-    public string DeleteEntities(string Id){
+    [Route("{Id:alpha}", Name = "DeleteById")]
+    public string DeleteEntities(string Id)
+    {
         var data = MockDatabase.Entities.FirstOrDefault(n => n.Id == Id);
         MockDatabase.Entities.Remove(data);
         return "Successfully Deleted" + Id;
