28ae87291603197b188d86e1cd63a5721a1eaa0c
Ashish Thakur <sendashishthaur@gmail.com>
2024-03-17 00:07:19 +0530

    swagger

--- EntityManagement.API/obj/Debug/net8.0/EntityManagement.API.AssemblyInfo.cs
+++ EntityManagement.API/obj/Debug/net8.0/EntityManagement.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntityManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a7e00b8cbca53580fedf987cac6280f2332f11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19874c312deb6cda320258a50123a7b0a81ef4bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntityManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntityManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
